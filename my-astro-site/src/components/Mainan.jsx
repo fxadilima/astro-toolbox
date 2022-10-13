67b628828c68975ca6bf8ef61446be694bc59c4d
fxadilima <107688563+fxadilima@users.noreply.github.com>
2022-10-13 20:57:35 +0700

    Add files via upload

---
+++ my-astro-site/src/components/Mainan.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Mainan = () => {
+    return (
+        <div className="w3-panel w3-card-4 w3-green">
+            <h3>Mainan</h3>
+            <p>
+                Card ini berasal dari sebuah komponen JSX bernama "Mainan".
+            </p>
+        </div>
+    );
+}
+
+export default Mainan;
